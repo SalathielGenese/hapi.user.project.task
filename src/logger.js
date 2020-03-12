1305706a5cb1bd528428dc780f60457439f35cac
Salathiel Genese <salathielgenese@gmail.com>
2020-03-12 22:02:15 +0400

    Implement logger logic into a module tto automate its configuration

---
+++ src/logger.js
@@ -0,0 +1,31 @@
+const { name: APP_NAME } = require( '../package' );
+const debug = require( 'debug' );
+
+
+
+/**
+ *
+ * Get a scoped logger
+ *
+ * @param { string } scope
+ * @return { function( ...parameters: any[] ) }
+ */
+function getLogger( scope )
+{
+    return function ( ...parameters )
+    {
+        const [ , callee ] = new Error().stack.split( /\r?\n/ )[ 2 ].match( CALLEE_REGEX );
+
+        debug( `${ APP_NAME }:${ callee }:${ scope }` )( ...parameters );
+    };
+}
+
+const { INIT_CWD } = process.env;
+const CALLEE_REGEX = new RegExp( `${ INIT_CWD.replace( /[-\[](){}:.]/g, '\\$1') }[\\/]\(.*\)\.js:\\d+:\\d+` );
+
+
+
+module.exports = {
+    debug: getLogger( 'debug' ),
+    error: getLogger( 'error' ),
+};
