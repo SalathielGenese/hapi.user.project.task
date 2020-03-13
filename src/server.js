3584f008ce0c494546e155e3c6584258e2095329
Salathiel Genese <salathielgenese@gmail.com>
2020-03-13 08:29:19 +0400

    Implement open/close route discovery and loading

--- src/server.js
+++ src/server.js
@@ -1,18 +1,48 @@
-const { HOST, PORT } = require( './env' );
+const { APP_ROOT, HOST, PORT } = require( './env' );
+const logger = require( './logger' );
 const Hapi = require( '@hapi/hapi' );
+const find = require( 'find' );
 
 
 
+const BASE_ROUTE_PATH = `${ APP_ROOT }/src`;
 const server = Hapi.server({ port: PORT, host: HOST });
 
-server.route({
-    method: 'GET',
-    path: '/api',
-    handler: ( request, h ) =>
+const METHODS = [ 'GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'PATCH' ];
+const files = find.fileSync( /[\\/.]route\.js$/, BASE_ROUTE_PATH );
+
+files
+    .map( file => file.substr( BASE_ROUTE_PATH.length, file.length ) )
+    .map(source => ({ source, path: source.replace( /[\\/]/g, '/' ).replace( /^(.*)[\\/.]route\.js$/, '$1' ) }) )
+    .forEach( ({ path, source }) =>
     {
-        return { a: 'Aa' };
-    }
-});
+        const module = require( `${ BASE_ROUTE_PATH }${ source }` );
+
+        'function' === typeof module
+            ? registerRoute( module )
+            : Object.keys( module )
+                .filter( key => 'function' === typeof module[ key ])
+                .map( key => module[ key ] )
+                .forEach( handler => registerRoute( path, handler ) );
+    });
+
+/**
+ *
+ * @param { string } path
+ * @param { function } handler
+ */
+function registerRoute( path, handler )
+{
+    const { _method = handler.name } = handler;
+    const method = METHODS.includes( _method.toUpperCase() ) ? _method.toUpperCase() : 'GET';
+
+    logger.debug( `${ method } "${ path }" (${ handler.name })` );
+    server.route({
+        handler,
+        method,
+        path,
+    });
+}
 
 
 
