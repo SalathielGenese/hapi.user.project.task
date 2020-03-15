f636571fc5f6bc228fe6e9740aad991abfd4f742
Salathiel Genese <salathielgenese@gmail.com>
2020-03-15 23:51:16 +0400

    Extract, process and fallback to default values, the database options from environment

--- src/env.js
+++ src/env.js
@@ -3,6 +3,14 @@ const { name: APP_NAME } = require( '../package' );
 
 
 const {
+    POSTGRES_DIALECT_OPTIONS = '{}',
+    POSTGRES_DATABASE = 'database',
+    POSTGRES_DIALECT = 'postgres',
+    POSTGRES_USERNAME = 'root',
+    POSTGRES_HOST = '0.0.0.0',
+    POSTGRES_PASSWORD = null,
+    POSTGRES_POST = 5432,
+
     DEBUG = `${ APP_NAME }:*`,
     INIT_CWD: APP_ROOT,
     HOST = '0.0.0.0',
@@ -14,6 +22,14 @@ process.env.DEBUG = DEBUG;
 
 
 module.exports = {
+    POSTGRES_DIALECT_OPTIONS,
+    POSTGRES_DATABASE,
+    POSTGRES_USERNAME,
+    POSTGRES_PASSWORD,
+    POSTGRES_DIALECT,
+    POSTGRES_HOST,
+    POSTGRES_POST,
+
     APP_NAME,
     APP_ROOT,
     PORT,
