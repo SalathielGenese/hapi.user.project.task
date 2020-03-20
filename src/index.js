e72f0a5aed8da0bda829d795340e8928a1117c7f
Salathiel Genese <salathielgenese@gmail.com>
2020-03-20 04:37:18 +0400

    FIX remove the force option to prevent table drop

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const logger = require( './logger' );
 
 ( async () =>
 {
-    await server.start();
     await sequelize.authenticate({ retry: 10 });
+    await sequelize.sync();
+    await server.start();
     logger.debug( `Web server started at ${ HOST }:${ PORT }` );
 })().catch( logger.error );
