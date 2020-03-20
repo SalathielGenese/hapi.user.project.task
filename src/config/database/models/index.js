99e09489615f526831a0e6b899f7b9ffa483ddd2
Salathiel Genese <salathielgenese@gmail.com>
2020-03-21 03:06:01 +0400

    Simplify code with find

--- src/config/database/models/index.js
+++ src/config/database/models/index.js
@@ -1,31 +1,20 @@
-'use strict';
+const config = require( '../../database' );
 const Sequelize = require( 'sequelize' );
-const path = require( 'path' );
-const fs = require( 'fs' );
+const find = require( 'find' );
 
 
 
+const sequelize = new Sequelize( config );
 const db = {};
-const basename = path.basename( __filename );
-const config = require( `${ __dirname }/../../database` );
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs.readdirSync( __dirname )
-    .filter( file => !file.startsWith( '.' ) && file.endsWith( '.js' ) && !( file === basename ) )
-    .forEach( file =>
-    {
-      const model = sequelize[ 'import' ]( path.join( __dirname, file ) );
-
-      db[ model.name ] = model;
-    });
-
-Object.keys( db ).forEach( modelName =>
-{
-  if ( db[ modelName ].associate )
-  {
-    db[ modelName ].associate( db );
-  }
-});
+find.fileSync( /\.js$/, __dirname )
+    .filter( file => file !== __filename )
+    .map( path => sequelize.import( path ) )
+    .forEach( model => db[ model.name ] = model );
+Object.keys( db )
+    .map( name => db[ name ] )
+    .filter( ({ associate }) => associate )
+    .forEach( model => model.associate( db ) );
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
